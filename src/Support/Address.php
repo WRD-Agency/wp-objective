7e7d1b59b60f8074f79c1bd7b2ad2c40fe273708
wrd-sodapixel <dev@sodapixel.com>
2025-11-06 11:36:05 +0000

    Allow partial addresses.

--- src/Support/Address.php
+++ src/Support/Address.php
@@ -22,79 +22,79 @@ class Address implements Apiable {
 	 *
 	 * A street address. When combined with address lines 2 & 3, this should fully identify the location of the address within its second administrative level (typically a city or town), but should not include the city name, ZIP or postal code, or country name.
 	 *
-	 * @var string
+	 * @var ?string
 	 */
-	private string $address_line_1;
+	private ?string $address_line_1;
 
 	/**
 	 * Address line 2.
 	 *
-	 * @var string|null
+	 * @var ?string
 	 */
 	private ?string $address_line_2;
 
 	/**
 	 * Address line 3.
 	 *
-	 * @var string|null
+	 * @var ?string
 	 */
 	private ?string $address_line_3;
 
 	/**
 	 * The first administrative level in the address. This is typically the province in which the address is located. In the United States, this would be the state. In Switzerland, the canton. In the United Kingdom, the county.
 	 *
-	 * @var string
+	 * @var ?string
 	 */
-	private string $administrative_level_1;
+	private ?string $administrative_level_1;
 
 	/**
 	 * The second administrative level, in addresses with at least two of them. In countries with two administrative levels, this would typically be the city, town, village, or other locality in which the address is located.
 	 *
-	 * @var string
+	 * @var ?string
 	 */
-	private string $administrative_level_2;
+	private ?string $administrative_level_2;
 
 	/**
 	 * A country or territory code.
 	 *
-	 * @var string
+	 * @var ?string
 	 */
-	private string $country_code;
+	private ?string $country_code;
 
 	/**
 	 * A postal code (in the United States, this is the ZIP code).
 	 *
-	 * @var string
+	 * @var ?string
 	 */
-	private string $postal_code;
+	private ?string $postal_code;
 
 	/**
 	 * The latitude of this address.
 	 *
-	 * @var string
+	 * @var ?string
 	 */
 	private ?string $lat = null;
 
 	/**
 	 * The longitude of this address.
 	 *
-	 * @var string
+	 * @var ?string
 	 */
 	private ?string $long = null;
 
 	/**
 	 * Create a new address.
 	 *
-	 * @param string      $address_line_1 Address line 1.
-	 * @param string|null $address_line_2 Address line 2.
-	 * @param string|null $address_line_3 Address line 3.
-	 * @param string      $administrative_level_1 The first administrative level in the address. This is typically the province in which the address is located. In the United States, this would be the state. In Switzerland, the canton. In the United Kingdom, the county.
-	 * @param string      $administrative_level_2 The second administrative level in the address. This is typically the city or town in which the address is located.
-	 * @param string      $country_code The country code for the address.
-	 * @param string      $postal_code The postal code for the address.
-	 * @param string[]    $coordinates The coordinates. Array with keys 'lat' and 'long'.
+	 * @param ?string   $address_line_1 Address line 1.
+	 * @param ?string   $address_line_2 Address line 2.
+	 * @param ?string   $address_line_3 Address line 3.
+	 * @param ?string   $administrative_level_1 The first administrative level in the address. This is typically the province in which the address is located. In the United States, this would be the state. In Switzerland, the canton. In the United Kingdom, the county.
+	 * @param ?string   $administrative_level_2 The second administrative level in the address. This is typically the city or town in which the address is located.
+	 * @param ?string   $country_code The country code for the address.
+	 * @param ?string   $postal_code The postal code for the address.
+	 * @param ?string[] $coordinates The coordinates. Array with keys 'lat' and 'long'.
 	 */
-	public function __construct( string $address_line_1, string $address_line_2, string $address_line_3, string $administrative_level_1, string $administrative_level_2, string $country_code, string $postal_code, ?array $coordinates ) {
+	public function __construct( ?string $address_line_1 = null, ?string $address_line_2 = null, ?string $address_line_3 = null, ?string $administrative_level_1 = null, ?string $administrative_level_2 = null, ?string $country_code = null, ?string $postal_code = null, ?array $coordinates = null ) {
 		$this->address_line_1         = $address_line_1;
 		$this->address_line_2         = $address_line_2;
 		$this->address_line_3         = $address_line_3;
@@ -128,10 +128,21 @@ class Address implements Apiable {
 			$components[] = $this->address_line_3;
 		}
 
-		$components[] = $this->administrative_level_1;
-		$components[] = $this->administrative_level_2;
-		$components[] = $this->country_code;
-		$components[] = $this->postal_code;
+		if ( $this->administrative_level_1 ) {
+			$components[] = $this->administrative_level_1;
+		}
+
+		if ( $this->administrative_level_2 ) {
+			$components[] = $this->administrative_level_2;
+		}
+
+		if ( $this->country_code ) {
+			$components[] = $this->country_code;
+		}
+
+		if ( $this->postal_code ) {
+			$components[] = $this->postal_code;
+		}
 
 		return implode( ', ', $components );
 	}
